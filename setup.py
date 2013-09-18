e85f12e579c9a6aadc414d8883413b0cf47153ee
ZHU Jie M <jie.m.zhu@alcatel-sbell.com.cn>
2013-09-18 16:59:50 +0800

    add setup scripts for cx-freeze

---
+++ setup.py
@@ -0,0 +1,27 @@
+from cx_Freeze import setup, Executable
+import sys
+sys.path.append('src')
+from main import REV
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = {'packages': ['urllib', 'PIL'],
+                'includes': ['win32.win32gui', 'log', 'record', 
+                              'webutil', 'setter', 'bingwallpaper'],
+                 'excludes': ['tkinter'],
+                 'compressed':1,
+                 'include_files': [('src/winsetter.py','')],
+                 'bin_includes': ['pywintypes33.dll'],
+                 'optimize': 2,
+               }
+
+executables = [
+    Executable('./src/main.py', base='Win32GUI', targetName='BingWallpaper.exe'),
+    Executable('./src/main.py', base='Console', targetName='BingWallpaper-cli.exe')
+]
+
+setup(name='PyBingWallpaper.exe',
+      version = REV,
+      description = 'Bing.com Wallpaper Downloader',
+      options = {'build_exe': buildOptions},
+      executables = executables)
